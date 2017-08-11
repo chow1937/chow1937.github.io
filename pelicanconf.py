2bd5bea145e2c4f782bc25f84ac37f7059457a55
tonychow <chow1937@gmail.com>
2017-08-11 15:17:55 +0800

    first publish for test.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'tonychow'
-SITENAME = u'Tonychow'
+SITENAME = u"Tonychow's Blog"
 SITEURL = ''
 
 PATH = 'content'
@@ -32,4 +32,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True

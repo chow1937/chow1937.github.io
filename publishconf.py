83c8a5a528190c0f3a08740a8cdc41efb326c10d
tonychow <chow1937@gmail.com>
2019-07-08 14:56:51 +0800

    update https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.tonychow.me'
+SITEURL = 'https://blog.tonychow.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
